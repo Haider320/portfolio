3f5330eef6727f6f7594f433163c4605400ca316
Haider320 <118187456+Haider320@users.noreply.github.com>
2023-06-28 12:33:27 +0600

    Add files via upload

---
+++ asset/js/app.js
@@ -0,0 +1,21 @@
+
+var typed = new Typed('.type', {
+  strings: [
+    'Web designer',
+     'Ui/Ux Designer',
+     'Freelancer'
+],
+  typeSpeed: 60,
+  backspeed: 60,
+  loop: true,
+});
+
+
+
+AOS.init({
+  offset: 200,
+  duration: 1000,
+});
+
+
+
